50b7c31712212225a1a5438f1b94d05e6d4ba4a0
MsMutanu <faithtanuh@gmail.com>
2021-08-23 18:29:17 +0300

    simple shell

---
+++ free_array.c
@@ -0,0 +1,21 @@
+#include "shed.h"
+
+/**
+ * free_array - free tokens in array and the array itself
+ *
+ * @array: array of pointers to tokens
+ */
+
+void free_array(char **array)
+{
+	int i = 0;
+
+	if (array == NULL)
+		return;
+	while (array[i] != NULL)
+	{
+		free(array[i]);
+		i++;
+	}
+	free(array);
+}
